777ade407ec85cbc62d7c814a4c0529a9d324bd4
Nelson <nfonkoua@weborama.com>
2021-04-09 19:28:51 +0200

    code reformat

--- src/main/test/com/tambapps/http/getpack/GetpackClientTest.groovy
+++ src/main/test/com/tambapps/http/getpack/GetpackClientTest.groovy
@@ -6,30 +6,29 @@ import static org.junit.jupiter.api.Assertions.assertNotNull
 
 
 class GetpackClientTest {
+  @Test
+  void testGet() {
+    GetpackClient client = new GetpackClient("https://jsonplaceholder.typicode.com")
 
-    @Test
-    void testGet() {
-        GetpackClient client = new GetpackClient("https://jsonplaceholder.typicode.com")
+    def todo = client.get("/todos/1")
 
-        def todo = client.get("/todos/1")
+    println(todo)
+    assertNotNull(todo.id)
+    assertNotNull(todo.userId)
+    assertNotNull(todo.title)
+    assertNotNull(todo.completed)
+  }
 
-        println(todo)
-        assertNotNull(todo.id)
-        assertNotNull(todo.userId)
-        assertNotNull(todo.title)
-        assertNotNull(todo.completed)
-    }
+  @Test
+  void testGetWithQueryParam() {
+    GetpackClient client = new GetpackClient("https://jsonplaceholder.typicode.com")
 
-    @Test
-    void testGetWithQueryParam() {
-        GetpackClient client = new GetpackClient("https://jsonplaceholder.typicode.com")
+    def todo = client.get("/todos/1", query: [a: 1, b: "2", c: "sisi"])
 
-        def todo = client.get("/todos/1", query: [a:1, b: "2", c: "sisi"])
-
-        println(todo)
-        assertNotNull(todo.id)
-        assertNotNull(todo.userId)
-        assertNotNull(todo.title)
-        assertNotNull(todo.completed)
-    }
+    println(todo)
+    assertNotNull(todo.id)
+    assertNotNull(todo.userId)
+    assertNotNull(todo.title)
+    assertNotNull(todo.completed)
+  }
 }
