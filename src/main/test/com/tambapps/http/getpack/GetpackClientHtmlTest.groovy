61aedc2bbea48942d0ac89ebea1b4211b09855a7
Nelson <nfonkoua@weborama.com>
2021-04-11 03:31:43 +0200

    added class for html test

---
+++ src/main/test/com/tambapps/http/getpack/GetpackClientHtmlTest.groovy
@@ -0,0 +1,23 @@
+package com.tambapps.http.getpack
+
+import okhttp3.ResponseBody
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.junit.jupiter.api.Test
+
+import static org.junit.jupiter.api.Assertions.assertEquals
+
+class GetpackClientHtmlTest {
+
+  private GetpackClient client = new GetpackClient(url: "https://fr.wikipedia.org",
+      contentType: ContentType.JSON, acceptContentType: ContentType.JSON).with {
+    decoders[ContentType.HTML] = { ResponseBody body -> Jsoup.parse(body.string()) }
+    it
+  }
+
+  @Test
+  void testJsoup() {
+    def html = client.get("/wiki/Groovy_(langage)")
+    assertEquals(Document.class, html.class)
+  }
+}
