925eb6a3b94fe823e817fb2ea3a17f9776a52a29
Tambapps <tambapps@gmail.com>
2022-04-18 22:17:23 +0200

    added constructors

--- src/main/java/com/tambapps/http/hyperpoet/FormPart.java
+++ src/main/java/com/tambapps/http/hyperpoet/FormPart.java
@@ -1,9 +1,13 @@
 package com.tambapps.http.hyperpoet;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 // TODO document this
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FormPart {
 
   // optional
