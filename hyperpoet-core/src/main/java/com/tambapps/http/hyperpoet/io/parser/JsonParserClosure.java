8a797525a2f462116d79355acb6ded54ddea468e
Nelson FONKOUA <nelson.fonkoua@vialink.fr>
2023-08-16 14:46:22 +0200

    fixed boolean poetic printing

--- hyperpoet-core/src/main/java/com/tambapps/http/hyperpoet/io/parser/JsonParserClosure.java
+++ hyperpoet-core/src/main/java/com/tambapps/http/hyperpoet/io/parser/JsonParserClosure.java
@@ -2,6 +2,7 @@ package com.tambapps.http.hyperpoet.io.parser;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import okhttp3.ResponseBody;
 
 import java.io.IOException;
@@ -32,26 +33,29 @@ public class JsonParserClosure extends ParserClosure {
     return toObject(node);
   }
 
+  @SneakyThrows
   public static Object toObject(JsonNode node) {
     if (node.isDouble()) return node.asDouble();
     else if (node.isFloat()) return node.floatValue();
     else if (node.isLong()) return node.asLong();
     else if (node.isInt()) return node.asInt();
+    else if (node.isBigInteger()) return node.bigIntegerValue();
     else if (node.isTextual()) return node.asText();
+    else if (node.isBoolean()) return node.asBoolean();
+    else if (node.isBinary()) return node.binaryValue();
+    else if (node.isNull()) return null;
     else if (node.isArray()) {
       List<Object> list = new ArrayList<>();
       for (int i = 0; i < node.size(); i++) {
         list.add(toObject(node.get(i)));
       }
       return list;
-    } else if (node.isEmpty()) {
-      return new HashMap<>();
     } else if (node.isObject()) {
       Map<String, Object> map = new HashMap<>();
       node.fields().forEachRemaining(e -> map.put(e.getKey(), toObject(e.getValue())));
       return map;
     } else {
-      throw new RuntimeException("Internal error, doesn't handle such node");
+      throw new RuntimeException("Internal error, doesn't handle node " + node);
     }
   }
 }
