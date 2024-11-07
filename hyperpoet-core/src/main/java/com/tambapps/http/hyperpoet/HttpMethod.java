9faed9995efd528c52aee4778108dfb8ba193d63
Nelson Fonkoua <tambapps@gmail.com>
2024-11-07 23:42:31 +0100

    fully implemented operation poetic invoker

--- hyperpoet-core/src/main/java/com/tambapps/http/hyperpoet/HttpMethod.java
+++ hyperpoet-core/src/main/java/com/tambapps/http/hyperpoet/HttpMethod.java
@@ -1,5 +1,9 @@
 package com.tambapps.http.hyperpoet;
 
 public enum HttpMethod {
-  POST, GET, PATCH, PUT, DELETE
+  POST, GET, PATCH, PUT, DELETE;
+
+  public boolean hasBody() {
+    return this == POST || this == PUT || this == PATCH;
+  }
 }
